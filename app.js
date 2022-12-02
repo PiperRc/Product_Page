0ab8e38167335b56a38f26b8cec0508d5f969c88
Cheyenne Piper <robeltopiper@gmail.com>
2022-12-02 01:20:57 -0500

    Added mobile navbar when hamburger is clicked

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const hamburger=document.querySelector('.hamburger');
-const hiddenNavbar=document.querySelector('.hidden-navbar');
+const hamburger = document.querySelector('.hamburger');
+const hiddenNavbar = document.querySelector('.hidden-navbar');
+const x =document.querySelector('.hidden-navbar img');
+const body=document.querySelector('body');
 
-hamburger.addEventListener('click',()=>{
+hamburger.addEventListener('click', () => {
+  
     hiddenNavbar.classList.add('hidden-navbar-reveal');
+    // body.classList.add('body-opacity')
+   
 })
+
+x.addEventListener('click',()=>{
+    hiddenNavbar.classList.remove('hidden-navbar-reveal');
+    
+})
+
+
 
