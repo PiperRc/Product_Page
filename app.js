9a61e11c678ef0e29e92027da93fe00335c485fd
Cheyenne Piper <robeltopiper@gmail.com>
2022-12-09 00:29:50 -0500

    Added cart feature

--- app.js
+++ app.js
@@ -1,84 +1,103 @@
-const body=document.querySelector('body');
-
+// hidden navbar
 const hamburger = document.querySelector('.hamburger');
-
 const hiddenNavbar = document.querySelector('.hidden-navbar');
-
-const hiddenLinks=document.querySelector('.hidden-links');
-
-const container= document.querySelector('.container');
-
-const leftArrow=document.querySelector('.previous-icon');
-
-const rightArrow=document.querySelector('.next-icon');
-
-const productImage=document.querySelector('.product-image img');
-
-const x =document.querySelector('.x-icon');
-
-let num=1;
-
-const imgSrc={
-    1:'images/image-product-1.jpg',
-    2:'images/image-product-2.jpg',
-    3:'images/image-product-3.jpg',
-    4:'images/image-product-4.jpg',
-}
-
-// container.addEventListener('click',()=>{
-//     if(hiddenNavbar.classList.contains('hidden-navbar-reveal')){
-//         hiddenNavbar.classList.remove('hidden-navbar-reveal');
-//         hiddenLinks.classList.remove('hidden-links-reveal')
-//         container.classList.remove('container-opacity');
-//     }
-// }
-// )
-
-
-
-// When the left arrow is clicked, decrease the imgSrc number,but cannot be less than 1
-// Whe the right arrow is clicked,increase the imgSrc number ,but cannot be more than 4
-
-
-productImage.addEventListener('click',()=>{
-    
-})
-
-leftArrow.addEventListener('click',()=>{
-    if(num>1){
-        --num;
-    }
-    productImage.src=imgSrc[num];
-})
-
-rightArrow.addEventListener('click',()=>{
-    if(num<4){
-        ++num
-    }
-
-    productImage.src=imgSrc[num];
-})
-
-
-
-
+const hiddenLinks = document.querySelector('.hidden-links');
+const x = document.querySelector('.x-icon');
+const container = document.querySelector('.container');
 
 hamburger.addEventListener('click', () => {
-  
+
     hiddenNavbar.classList.add('hidden-navbar-reveal');
     hiddenLinks.classList.add('hidden-links-reveal')
-  
+
     container.classList.add('container-opacity');
-   
+
 })
 
-x.addEventListener('click',()=>{
+x.addEventListener('click', () => {
     hiddenNavbar.classList.remove('hidden-navbar-reveal');
     hiddenLinks.classList.remove('hidden-links-reveal')
     container.classList.remove('container-opacity');
-    
+
 })
 
 
+// hero image
+const leftArrow = document.querySelector('.previous-icon');
 
+const rightArrow = document.querySelector('.next-icon');
+
+const productImage = document.querySelector('.product-image img');
+
+let imgNum = 1;
+
+const imgSrc = {
+    1: 'images/image-product-1.jpg',
+    2: 'images/image-product-2.jpg',
+    3: 'images/image-product-3.jpg',
+    4: 'images/image-product-4.jpg',
+
+}
+leftArrow.addEventListener('click', () => {
+    if (imgNum > 1) {
+        --imgNum;
+    }
+    else if (imgNum === 1) {
+        imgNum = 4;
+    }
+
+    productImage.src = imgSrc[imgNum];
+
+})
+
+rightArrow.addEventListener('click', () => {
+    if (imgNum < 4) {
+        ++imgNum;
+    }
+
+    else if (imgNum === 4) {
+        imgNum = 1;
+    }
+
+    productImage.src = imgSrc[imgNum];
+
+})
+
+
+// cart
+const minusSign = document.querySelector('.minus-sign');
+const plusSign = document.querySelector('.plus-sign');
+
+const cartSpan = document.querySelector('.nav-cart-span');
+
+const cartNumSpan = document.querySelector('.product-amount span');
+
+let cartNum = parseInt(document.querySelector('.product-amount span').textContent);
+
+const cartAddition = document.querySelector('.cart-addition')
+
+
+minusSign.addEventListener('click', () => {
+
+    if (cartNum > 0) {
+        --cartNum;
+    }
+
+    cartNumSpan.textContent = cartNum;
+
+
+})
+
+plusSign.addEventListener('click', () => {
+
+    ++cartNum
+    cartNumSpan.textContent = cartNum;
+
+})
+
+cartAddition.addEventListener('click', () => {
+    cartSpan.innerHTML=cartNum;
+    cartSpan.classList.add('nav-cart-span-reveal');
+
+})
 
